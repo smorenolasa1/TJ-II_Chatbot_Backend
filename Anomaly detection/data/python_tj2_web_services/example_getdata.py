4052cdc5b09bb6c32ca410f09ed64ddec93f9138
smorenolasa1 <smoreno.ieu2021@student.ie.edu>
2025-02-05 11:32:31 +0100

    clean data

--- Anomaly detection/data/python_tj2_web_services/example_getdata.py
+++
@@ -1,8 +0,0 @@
-import tj2services
-
-import matplotlib.pyplot as plt
-
-r = tj2services.getdata(shot=50000, signal='ACTON275')
-
-plt.plot(r['time'], r['data'])
-plt.show()
--- Anomaly detection/data/python_tj2_web_services/example_magnetic.py
+++
@@ -1,5 +0,0 @@
-import tj2services
-
-r = tj2services.flux_car(config='100_44_64', x=1.7, y=0, z=0.05)
-
-print(f'Value of the flux for (x={r["x"]}, y={r["y"]}, z={r["z"]}):', r['psi'])
