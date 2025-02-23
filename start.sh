2ad67459ac5cf7a2445668697077b03ca0aff733
smorenolasa1 <smoreno.ieu2021@student.ie.edu>
2025-02-23 19:25:54 +0100

    reduce memory usage for render

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# Install spaCy model before starting FastAPI
+
+# Install the spaCy model (if not already installed)
 python -m spacy download es_core_news_sm
-uvicorn main:app --host 0.0.0.0 --port $PORT
+
+# Start FastAPI with only 1 worker to reduce memory usage
+uvicorn main:app --host 0.0.0.0 --port $PORT --workers 1
